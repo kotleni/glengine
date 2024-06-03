81de9d092800c02bd5ee8ad06c7ad42549967944
Viktor Varenik <yavarenikya@gmail.com>
2024-06-03 18:29:04 +0300

    refactor

--- inc/define.hpp
+++ inc/define.hpp
@@ -5,6 +5,19 @@
     #define OS_64BIT
 #endif
 
+// Detect OS
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #define OS_WINDOWS
+#elif defined(__linux__)
+    #define OS_LINUX
+#elif defined(__APPLE__) && defined(__MACH__)
+    #define OS_MACOS
+#elif defined(unix) || defined(__unix__) || defined(__unix)
+    #define OS_UNIX
+#else
+    #error Unknown environment!
+#endif
+
 // Engine defenitions
 #define ENGINE_NAME "engine"
 #define ENGINE_GLSL_VERSION "#version 130"
