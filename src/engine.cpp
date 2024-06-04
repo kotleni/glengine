5ea8e28d9f46a2b2b3c87cb45a805b4431aeb44f
Viktor Varenik <yavarenikya@gmail.com>
2024-06-04 10:35:00 +0300

    fun: a lot of cubes

--- src/engine.cpp
+++ src/engine.cpp
@@ -230,6 +230,19 @@ glm::mat4 proj;
 glm::mat4 model;
 glm::mat4 view;
 glm::mat4 trans;
+glm::vec3 cubePositions[] = {
+glm::vec3( 0.0f, 0.0f,
+ 0.0f),
+glm::vec3( 2.0f, 5.0f, -15.0f),
+glm::vec3(-1.5f, -2.2f, -2.5f),
+glm::vec3(-3.8f, -2.0f, -12.3f),
+glm::vec3( 2.4f, -0.4f, -3.5f),
+glm::vec3(-1.7f, 3.0f, -7.5f),
+glm::vec3( 1.3f, -2.0f, -2.5f),
+glm::vec3( 1.5f, 2.0f, -2.5f),
+glm::vec3( 1.5f, 0.2f, -1.5f),
+glm::vec3(-1.3f, 1.0f, -1.5f)
+};
 
 void begin_run() {
 	proj = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
@@ -277,6 +290,7 @@ void Engine::on_event(SDL_Event *event) {
     // todo
 }
 
+float offset = 0;
 void Engine::on_render() {
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 	glClearColor(clear_color.x * clear_color.w,
@@ -286,7 +300,16 @@ void Engine::on_render() {
 
 	texturedShader->use();
 
-	model = glm::rotate(model, (float)glm::radians(4.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+	offset += 0.09;
+
+	glBindVertexArray(VAO);
+for(unsigned int i = 0; i < 10; i++)
+{
+glm::mat4 model = glm::mat4(1.0f);
+model = glm::translate(model, cubePositions[i]);
+float angle = 20.0f * offset;
+model = glm::rotate(model, glm::radians(angle),
+glm::vec3(1.0f, 0.3f, 0.5f));
 
 	unsigned int projectionLoc = glGetUniformLocation(texturedShader->program, "projection");
 	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(proj));
@@ -299,6 +322,8 @@ void Engine::on_render() {
 	glBindTexture(GL_TEXTURE_2D, defaultTexture);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	// use indicies: glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+}
 }
 
 void Engine::on_render_gui() {
